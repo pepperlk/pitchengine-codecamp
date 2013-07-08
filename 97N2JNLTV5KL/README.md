21971be43756a05e33c20ef7d0aeea61643ac2c4
Lee Pepper <pepperlk@gmail.com>
2013-07-08 18:02:00 -0400

    pepperlk Auto

--- 97N2JNLTV5KL/README.md
+++ 97N2JNLTV5KL/README.md
@@ -1,6 +1,16 @@
 # PitchEngine Code Camp Summer 2013
 # Cameron Whiting
-<!-- stuff goes here, correct? -->
-<!--also, can I use the terminal at all, or no? -->
-<!--current terminal usage: ls, wall, echo-->
-<!--yes, that was me.-->
+I will uncomment the twitter bootstrap later.
+I will probably use .delay functions to change the background (.css)
+and match that to a .delay function to fade in a transparent div (.fadeTo)
+
+I still don't know what's up with c9's .slideDown and .slideUp,
+and yes, the site delivers insecure content from my ip... yeah...
+
+
+so, since the opacity property is kind-of ireversable on the children
+of an element, I'm going to have to position the carousel manually
+ontop of it's (no longer) parent div. so, yeah :D
+
+derp. I'm going to switch the wrappers so the div in the back is transparent.
+derp.
--- 97N2JNLTV5KL/script.js
+++ 97N2JNLTV5KL/script.js
@@ -1,22 +1,48 @@
 $(document).ready(function() {
-    $('#title').fadeIn(1000);
-    setTimeout(function() {
-        $('#navbar').slideDown(1000);
+
+    //clickable buttons
+    
+    $('#termsopen').click(function(){
+        $('#termblob').fadeIn(100);
+    });
+    $('#termsclose').click(function(){
+        $('#termblob').fadeOut(50);
+    });
+    $('#previous').click(function(){
+        ('#carousel').carosel('previous'); //it's just gibberish... deal.
+    });
+    $('#next').click(function(){
+        ('#carousel').carousel('next');
+    });
+    
+    //initial ninja awesome page loading
+    
+    setTimeout(function(){
+        $('#title').fadeIn(100);
     }, 500);
+    setTimeout(function() {
+        $('#navbar').fadeIn(1000);
+    }, 1000);
     setTimeout(function() {
         $('#content').fadeIn(1000);
     }, 2000);
-    /*$('#carosel').setInterval(function(){
-        $('link[id="style"]').html('rel', 'starter.css');
-        setTimeout(function() {
-            $('link[id="style"]').html('rel', 'amber.css');
-        }, 2000);
-        setTimeout(function() {
-            $('link[id="style"]').html('rel', 'steel.css');
-        }, 2000);
-        setTimeout(function() {
-            $('link[id="style"]').html('rel', 'grass.css');
-        })
-    }, 60000);*/
+    setTimeout(function() {
+        $('#backing').fadeIn(1000);
+    }, 2000);
+    setTimeout(function() {
+        $('#footer').fadeIn(1000);
+    }, 3000);
+    
+    //background switcher, independant of carousel (for now)
+    
+        //the actual css background change (it doesn't actually work):
+        $('body').delay(5000).css('background-image', 'url(\'http://69.145.68.236:10200/amber.jpg\')').delay(5000).css('background-image', 'url(\'http://69.145.68.236:10200/gray.jpg\')').delay(5000).css('background-image', 'url(\'http://69.145.68.236:10200/steel.jpg\')');
+    
+        //and a div i'm using to hide it while it swaps out:
+        //$('hidebackground').fadeTo(100, 'slow' ).delay(400).;
+    
+    //adhoc carousel code
+        //will get back to this later...
+    
 });
 /*please don't change this*/
